820343009a364650263afa51c82d6f5ec0c6c0ef
Gerardo Manzo <programmingmanzo@gmail.com>
2020-03-23 22:44:19 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get 'hello_world', to  :hello_world
+get 'hello_world', to  ':hello#hello_world'
+end
 end
